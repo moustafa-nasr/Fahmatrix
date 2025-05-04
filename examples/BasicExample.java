9654ca5e6199acbb920bff35638ec6d0f41d6993
moustafa-nasr <moustafa.nasr.safan@gmail.com>
2025-05-04 16:37:06 +0300

    Add examples

---
+++ examples/BasicExample.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import com.fahmatrix.DataFrame;
+import com.fahmatrix.Series;
+
+public class BasicExample {
+    /* 
+     * Windows Only
+     * javac -cp ".\build\libs\fahmatrix-0.1.0.jar" .\examples\BasicExample.java
+     * java -cp ".\build\libs\fahmatrix-0.1.0.jar;examples" BasicExample
+     *
+     */
+    
+    public static void main(String[] args) {
+        DataFrame df = new DataFrame();
+		df.addColumn("age", Arrays.asList(25, 30, 35));
+		df.addColumn("name", Arrays.asList("Alice", "Bob", "Charlie"));
+
+        Series ages = df.getColumn("name");
+        df.print();
+        ages.print();
+    }
+}
